66ce502be3652acd96b46987e1391421514425bf
Yazan Ajlani <ajla0002@algonquinlive.com>
2013-12-06 08:00:11 -0500

    Initial Commit

--- js/main.js
+++ js/main.js
@@ -1,162 +1,142 @@
-var webworksreadyFired = false,
-				darkColoring = false,
-				darkScreenColor = 'black';
+contacts = {};
+contacts.db = window.openDatabase('contact database', '1.0', 'Contact information', 5*1024*1024);
+	var contact_firstname = "";
+	var contact_lastname = "";
+	var contact_mobilephone = "";
+	var contact_homephone = "";
+	var contact_workemail = "";
+	var contact_homeemail = "";
+	var contact_howwemet = "";
 
-			window.addEventListener('load',function() {
-				
-				createDB();
-				getMovie();
-				
-				
-				document.addEventListener('webworksready', function(e) {
-
-					if (webworksreadyFired) return;
-					webworksreadyFired = true;
-
-					var config;
-
-					if (darkColoring) {
-						config = {controlsDark: true,
-								listsDark: true};
-					} else {
-						config = {controlsDark: false,
-								listsDark: false,
-								coloredTitleBar: true};
-					}
-
-					config.onscreenready = function(element, id) {
-												if (darkColoring) {
-													var screen = element.querySelector('[data-bb-type=screen]');
-													if (screen) {
-														screen.style['background-color'] = darkScreenColor;
-													}
-												}
-											};
-					config.ondomready = function(element, id, params) {
-												if (id == 'dataOnTheFly') {
-													dataOnTheFly_initialLoad(element);
-												} 
-											};
-					bb.init(config);
-					bb.pushScreen('results.htm', 'results');
-				}, false);
-
-				if (navigator.userAgent.indexOf('BB10') < 0) {
-					var evt = document.createEvent('Events');
-					evt.initEvent('webworksready', true, true);
-					document.dispatchEvent(evt);
-				}
-			}, false);
-			
-			
-// encapsulate a global variable into a container so that we can access the database
-moviesdb = {};
-// create a database called movie database, with a version of 1.0, a display title of 'Offline Movie Information', and a size of 5GB
-moviesdb.db = window.openDatabase('movie database', '1.0', 'Offline Movie Information', 5*1024*1024);
-var movie_name = "";
-var movie_rev = "";
-
-// createDB will store the information
 function createDB() {
-	var db = moviesdb.db;
+	var db = contacts.db;
 	db.transaction(function(tx) {
-		tx.executeSql("CREATE TABLE IF NOT EXISTS Movies(ID INTEGER PRIMARY KEY ASC, name TEXT, review TEXT, added_on DATETIME)", 
-        	[], 
-			
-        	function(trans, result) {
-           // handle  the success
-            	console.log("Table created!");
-        	}, 
-			
-        	function(trans, error) {
-            // handle the error
-            	console.log("Table creation error: " + error);
-        	}
+		tx.executeSql("CREATE TABLE IF NOT EXISTS Contacts(ID INTEGER PRIMARY KEY ASC, firstname TEXT, lastname TEXT, mobilephone TEXT, homephone TEXT, workemail TEXT, homeemail TEXT, howwemet TEXT)",[],
+			function(tx, result) {
+				alert("Table created!");
+			},
+			function(tx, error) {
+				alert("Table creation error: " + error);
+			}
 		);
 	});
-		
 }
 
-// addMovie will instert adta into the table
-function addMovie(movie_name, movie_rev) {
+function addContact(contact_firstname, contact_lastname, contact_mobilephone, contact_homephone, contact_workemail, contact_homeemail, contact_howwemet) {
+
+	var db = contacts.db;
+
+	db.transaction(
+	function(tx) { tx.executeSql("INSERT INTO Contacts(firstname, lastname, mobilephone, homephone, workemail, homeemail, howwemet) VALUES (?, ?, ?, ?, ?, ?, ?) ",
+			[contact_firstname, contact_lastname, contact_mobilephone, contact_homephone, contact_workemail, contact_homeemail, contact_howwemet],
+			function(tx, result) {
+				alert("Contact added: " + contact_firstname + contact_lastname + contact_mobilephone + contact_homephone + contact_workemail + contact_homeemail + contact_howwemet);
+			},
 	
-	
-	var db = moviesdb.db;
-	
-	db.transaction(function(tx) {
-    	   var addedOn = new Date();
-     	   tx.executeSql("INSERT INTO Movies(name, review, added_on) VALUES (?, ?, ?) ",
-      	      [movie_name, movie_rev, addedOn],
-      	      function(trans, result) {
-        	        // handle the success
-            	    console.log("Movie added: " + movie_name + movie_rev);
-            	}, 
-            	function(trans, error) {
-                 // handle the error
-                	 console.log("Error adding " + movie_name + movie_rev + ": " + error);
-    	        }
-			);
+			function(tx, error) {
+				alert("Error adding " + contact_firstname + contact_lastname + contact_mobilephone + contact_homephone + contact_workemail + contact_homeemail + contact_howwemet + ": " + error);
+			}
+		);
 	});
 }
 
-// addNewMovie will add a movie to the results page when "add" the button is clicked on the add page
-function addNewMovie(){
-				var movieName = document.getElementById("inputMovie").value;
-				var movieReview = document.getElementById("inputReview").value;
-				console.log (movieName + " " + movieReview);
-		
-		addMovie(movieName, movieReview);
-		
-	}
+function addNewContact(){
+	
+		var contactfirstname = document.getElementById("inputFirstName").value;
+		var contactlastname = document.getElementById("inputLastName").value;
+		var contactmobilephone = document.getElementById("inputMobilePhone").value;
+		var contacthomephone = document.getElementById("inputHomePhone").value;
+		var contactworkemail = document.getElementById("inputWorkEmail").value;
+		var contacthomeemail = document.getElementById("inputHomeEmail").value;
+		var contacthowwemet = document.getElementById("inputHowWeMet").value;
 
+		addContact(contactfirstname, contactlastname, contactmobilephone, contacthomephone, contactworkemail, contacthomeemail, contacthowwemet);
+	
+}
 
-//getMovie will get the data out of the table
-function getMovie() {
-	var db = moviesdb.db;
+function getContacts() {
+	
+	var db = contacts.db;
 	var ul = document.createElement("ul");
 	
 	db.transaction(function(tx) {
-        	tx.executeSql("SELECT * FROM Movies",
-        	[],
-        	function(trans, result) {
-            	var movies = "";
-				var reviews = "";
-			 
-            	for(var i=0; i < result.rows.length; i++) {
-                	movies = result.rows.item(i).name + " - " + result.rows.item(i).review;
-		      		//reviews = result.rows.item(i).review + " ";
-				
+		tx.executeSql("SELECT * FROM Contacts",
+		[],
+		function(tx, result) {
+			var firstname = "";
+			var lastname = "";
+			var mobilephone = "";
+			var homephone = "";
+			var workemail = "";
+			var homeemail = "";
+			var howwemet = "";
+			
+			for(var i=0; i < result.rows.length; i++) {
+                	contacts = result.rows.item(i).firstname + " - " + result.rows.item(i).lastname + " - " + result.rows.item(i).mobilephone + " - " + result.rows.item(i).homephone + " - " + result.rows.item(i).workemail + " - " + result.rows.item(i).homeemail + " - " + result.rows.item(i).howwemet;
+					
+					
 					var li = document.createElement("li");
-					var txt = document.createTextNode(movies);
-					
-					//var li2 = document.createElement("li");
-					//var txt2 = document.createTextNode(reviews);
-					
+					var txt = document.createTextNode(contacts);
 					li.appendChild(txt);
-					//li2.appendChild(txt2);
-					
 					ul.appendChild(li);
-					//ul.appendChild(li2);
-				}
-				
-				
-				if(document.getElementById("outputDiv"))
-				{
-					//console.log("WIN");
-					var output = document.getElementById("outputDiv");
-					output.appendChild(ul);
-					//document.body.appendChild(ul);
-				}
-				
-
-			},
-			function(trans, error) {
-           		console.log("test2");
-				// handle the error
-				
-        	}
+			}
+			
+			if(document.getElementById("outputDiv")){
+				var output = document.getElementById("outputDiv");
+				output.appendChild(ul);
+			}
+			alert("got contacts");
+			
+		},
+		function (tx, error) {
+			alert("error");
+		}
 		);
-    });
+	});
+}
+	
+function allContactsClick(){
+	//bb.pushScreen('results.htm', 'contacts');
+	//getContacts();
+	$("#inputDiv").hide();
+	$("#outputDiv").show();
+}
+function addContactsClick(){
+	//bb.pushScreen('add.htm', 'add');
+	$("#outputDiv").hide();
+	$("#inputDiv").show();
 }
 
+function openWebLink() {
+    // open web link - allows the system to choose an appropriate target that handles http://
+    blackberry.invoke.invoke({
+		action: "bb.action.OPEN",
+        uri: "http://www.octranpso1.com/mobi"
+    }, onInvokeSuccess, onInvokeError);
+}
 
+function openWebLinkInBrowser() {
+    // open web link in browser
+    blackberry.invoke.invoke({
+		action: "bb.action.OPEN",
+        target: "sys.browser",
+        uri: "http://www.octranpso1.com/mobi"
+    }, onInvokeSuccess, onInvokeError);
+}
+
+function addToBbContacts() {
+	blackberry.invoke.invoke({
+		action: "bb.action.ADDTOCONTACT",
+		target: "sys.pim.contacts.app",
+		type: "application/vnd.blackberry.string.firstname, application/vnd.blackberry.string.lastname, application/vnd.blackberry.string.mobilephone, application/vnd.blackberry.string.homephone, application/vnd.blackberry.string.email1, application/vnd.blackberry.string.email2"
+	}, onInvokeSuccess, onInvokeError);
+}
+
+function onInvokeSuccess(response) {
+ console.log("<p>Invocation query successful: " + response + "</p>");
+}
+
+function onInvokeError(error) {
+ console.log("<p>Invocation query error: " + error + "</p>");
+}
+	
